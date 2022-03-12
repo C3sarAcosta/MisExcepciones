90ce2a5b245f236c376ca0f4b259ad46c26949da
César Acosta <c3sar.adan@gmail.com>
2022-03-11 21:13:52 -0700

    Agregar archivos de proyecto.

---
+++ MisExcepciones/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisExcepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program MyObject = new Program();
+                MyObject.ThrowException();
+                Console.ReadKey();
+            }
+            catch(MyException CaughtException)
+            {
+                Console.WriteLine(CaughtException.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public void ThrowException()
+        {
+            //Iniciamos nuestras propias excepciones con throw
+            throw new MyException();
+        }
+    }
+
+    class MyException : ApplicationException
+    {
+        public MyException() : base("This is my exception message")
+        {
+
+        }
+    }
+}
